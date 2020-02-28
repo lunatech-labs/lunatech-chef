32aa58b755c03075d09646a9a12011bed642ab3d
Leonor Boga <leonor.boga@lunatech.com>
2020-02-28 10:27:20 +0100

    Setup Flyway DB evolutions

--- src/Application.kt
+++ src/Application.kt
@@ -2,22 +2,46 @@ package com.lunatech
 
 import io.ktor.application.*
 import io.ktor.response.*
-import io.ktor.request.*
 import io.ktor.features.*
 import io.ktor.routing.*
 import io.ktor.http.*
 import io.ktor.auth.*
 import com.fasterxml.jackson.databind.*
+import com.typesafe.config.Config
+import com.typesafe.config.ConfigFactory
+import io.github.config4k.*
 import io.ktor.jackson.*
 import io.ktor.client.*
 import io.ktor.client.engine.apache.*
 import io.ktor.client.features.logging.*
+import org.flywaydb.core.Flyway
+
+data class FlywayConfig(val url: String, val user: String, val password: String) {
+    companion object {
+        fun fromConfig(config: Config): FlywayConfig {
+            val url: String by config
+            val user: String by config
+            val password: String by config
+
+            return FlywayConfig(
+                url,
+                user,
+                password
+            )
+        }
+    }
+}
+
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+    val flywayConfig = FlywayConfig.fromConfig(ConfigFactory.load().getConfig("flyway"))
+    val flyway = Flyway.configure().dataSource(flywayConfig.url, flywayConfig.user, flywayConfig.password).load();
+    flyway.migrate();
+
     install(CORS) {
         method(HttpMethod.Options)
         method(HttpMethod.Put)
@@ -67,4 +91,3 @@ fun Application.module(testing: Boolean = false) {
 
 class AuthenticationException : RuntimeException()
 class AuthorizationException : RuntimeException()
-

eedc1463ee7a3af4a79e63ae1d399882aa1dd4b3
Leonor Boga <leonor.boga@lunatech.nl>
2022-06-18 17:34:50 +0200

    Add the possibility to create a recorrent schedule

--- frontend/src/redux/schedules/SchedulesActionCreators.js
+++ frontend/src/redux/schedules/SchedulesActionCreators.js
@@ -63,12 +63,43 @@ export const addNewSchedule = (newSchedule) => (dispatch) => {
   axiosInstance
     .post("/schedules", scheduleToAdd)
     .then((response) => {
+      console.log("newSchedule.recurrency " + newSchedule.recurrency);
+      console.log(
+        "parseInt(newSchedule.recurrency, 10) " +
+          parseInt(newSchedule.recurrency, 10)
+      );
+
+      if (newSchedule.recurrency > 0) {
+        dispatch(addNewRecurrentSchedule(newSchedule));
+      }
+    })
+    .then((response) => {
       dispatch(fetchSchedules());
       dispatch(fetchSchedulesAttendance());
       dispatch(fetchAttendanceUser(userUuid));
     })
     .catch(function (error) {
       console.log("Failed adding Schedule: " + error);
+      dispatch(scheduleAddingFailed(error.message));
+    });
+};
+
+export const addNewRecurrentSchedule = (newRecurrentSchedule) => (dispatch) => {
+  console.log("addNewRecurrentSchedule addNewRecurrentSchedule");
+  const scheduleToAdd = {
+    menuUuid: newRecurrentSchedule.menuUuid,
+    locationUuid: newRecurrentSchedule.locationUuid,
+    repetitionDays: newRecurrentSchedule.recurrency,
+    nextDate: newRecurrentSchedule.date,
+  };
+
+  axiosInstance
+    .post("/recurrentschedules", scheduleToAdd)
+    .then((response) => {
+      // dispatch(fetchRecurrentSchedules());
+    })
+    .catch(function (error) {
+      console.log("Failed adding Recurrent Schedule: " + error);
       dispatch(scheduleAddingFailed(error.message));
     });
 };
@@ -95,8 +126,6 @@ export const editSchedule = (editedSchedule) => (dispatch) => {
 };
 
 export const deleteSchedule = (scheduleUuid) => (dispatch) => {
-  console.log("lets remove a schedule");
-
   axiosInstance
     .delete("/schedules/" + scheduleUuid)
     .then((response) => {

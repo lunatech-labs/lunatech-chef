14f1d7bcd5ff4b7057d4d3eae27cd0bc484bb411
Leonor Boga <leonor.boga@lunatech.nl>
2022-11-30 16:06:23 +0100

    FE - improve Location's list page css

--- frontend/src/components/admin/locations/ListLocations.js
+++ frontend/src/components/admin/locations/ListLocations.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 import Table from "react-bootstrap/Table";
 import Button from "react-bootstrap/Button";
 import { Loading } from "../../shared/Loading";
@@ -10,11 +12,11 @@ export default function ListLocations(props) {
   function ShowError({ error, reason }) {
     if (error) {
       return (
-        <div>
+        <Row>
           <h4>
             An error ocurred when {reason} a location: {error}
           </h4>
-        </div>
+        </Row>
       );
     } else {
       return <div></div>;
@@ -24,62 +26,58 @@ export default function ListLocations(props) {
   function RenderData({ isLoading, error, locations }) {
     if (isLoading) {
       return (
-        <div className="container">
-          <div className="row">
-            <Loading />
-          </div>
-        </div>
+        <Row>
+          <Loading />
+        </Row>
       );
     } else if (error) {
       return (
-        <div>
+        <Row>
           <h4>An error ocurred when feching Locations from server: {error}</h4>
-        </div>
+        </Row>
       );
     } else {
       return (
-        <div className="container">
-          <div className="row">
-            <Table striped bordered hover>
-              <thead>
-                <tr>
-                  <th>City</th>
-                  <th>Country</th>
-                  <th></th>
-                  <th></th>
-                </tr>
-              </thead>
-              <tbody>
-                {locations.map((loc) => {
-                  return (
-                    <tr key={loc.uuid}>
-                      <td>{loc.city}</td>
-                      <td>{loc.country}</td>
-                      <td>
-                        <Button
-                          variant="primary"
-                          value={loc.uuid}
-                          onClick={() => handleEdit(loc)}
-                        >
-                          <FontAwesomeIcon icon={faEdit} />
-                        </Button>
-                      </td>
-                      <td>
-                        <Button
-                          variant="danger"
-                          value={loc.uuid}
-                          onClick={() => handleRemove(loc.uuid)}
-                        >
-                          <FontAwesomeIcon icon={faMinus} />
-                        </Button>
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </Table>
-          </div>
-        </div>
+        <Row>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>City</th>
+                <th>Country</th>
+                <th></th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {locations.map((loc) => {
+                return (
+                  <tr key={loc.uuid}>
+                    <td>{loc.city}</td>
+                    <td>{loc.country}</td>
+                    <td>
+                      <Button
+                        variant="primary"
+                        value={loc.uuid}
+                        onClick={() => handleEdit(loc)}
+                      >
+                        <FontAwesomeIcon icon={faEdit} />
+                      </Button>
+                    </td>
+                    <td>
+                      <Button
+                        variant="danger"
+                        value={loc.uuid}
+                        onClick={() => handleRemove(loc.uuid)}
+                      >
+                        <FontAwesomeIcon icon={faMinus} />
+                      </Button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+        </Row>
       );
     }
   }
@@ -95,28 +93,29 @@ export default function ListLocations(props) {
   }
 
   return (
-    <div className="container">
-      <div>
+    <Container>
+      <Row>
         <h3 className="mt-4">Management of Locations</h3>
-      </div>
-      <Link to={`/newlocation`}>
-        <button type="button" className="btn btn-success">
-          <i>
-            <FontAwesomeIcon icon={faPlus} />
-          </i>{" "}
-          New Location
-        </button>
-      </Link>
-      <div>
-        <RenderData
-          isLoading={props.isLoading}
-          error={props.errorListing}
-          locations={props.locations}
-        />
-        <ShowError error={props.errorAdding} reason="adding" />
-        <ShowError error={props.errorDeleting} reason="deleting" />
-        <ShowError error={props.errorEditing} reason="saving" />
-      </div>
-    </div>
+      </Row>
+      <Row>
+        <Link to={`/newlocation`}>
+          <button type="button" className="btn btn-success">
+            <i>
+              <FontAwesomeIcon icon={faPlus} />
+            </i>{" "}
+            New Location
+          </button>
+        </Link>
+      </Row>
+      <Row></Row>
+      <RenderData
+        isLoading={props.isLoading}
+        error={props.errorListing}
+        locations={props.locations}
+      />
+      <ShowError error={props.errorAdding} reason="adding" />
+      <ShowError error={props.errorDeleting} reason="deleting" />
+      <ShowError error={props.errorEditing} reason="saving" />
+    </Container>
   );
 }

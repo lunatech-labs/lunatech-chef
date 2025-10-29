b2de28d4c6301eec49757884ea8f5a03ff79198b
Willem Jan Glerum <willem.jan.glerum@lunatech.nl>
2025-10-29 11:18:57 +0100

    Leverage Ktor plugin to set versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 import com.github.gradle.node.npm.task.NpmTask
 
 plugins {
+    id("io.ktor.plugin") version "3.3.1"
     id("com.github.node-gradle.node") version "7.1.0"
     id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
     id("org.jetbrains.kotlin.jvm") version "2.1.20"
     id("org.jetbrains.kotlin.plugin.serialization") version "2.2.21"
     id("com.github.autostyle") version "4.0"
-    id("application")
 }
 
 application {
@@ -36,7 +36,7 @@ dependencies {
     implementation("org.apache.logging.log4j:log4j-core:2.23.1")
 
     implementation("com.google.api-client:google-api-client:2.7.0")
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.18.2")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
     implementation("io.github.config4k:config4k:0.7.0")
     implementation("org.flywaydb:flyway-core:11.15.0")
     implementation("org.flywaydb:flyway-database-postgresql:10.18.0")
@@ -45,26 +45,26 @@ dependencies {
     implementation("org.postgresql:postgresql:42.7.8")
     implementation("org.quartz-scheduler:quartz:2.5.1")
 
-    implementation("io.ktor:ktor-server-status-pages:3.0.3")
-    implementation("io.ktor:ktor-server-default-headers:3.0.3")
-    implementation("io.ktor:ktor-server-cors:3.0.3")
-    implementation("io.ktor:ktor-server-content-negotiation-jvm:3.0.3")
-    implementation("io.ktor:ktor-server-netty-jvm:3.0.3")
-    implementation("io.ktor:ktor-server-core-jvm:3.0.3")
-    implementation("io.ktor:ktor-server-host-common-jvm:3.0.3")
-    implementation("io.ktor:ktor-server-auth:3.0.3")
-    implementation("io.ktor:ktor-server-auth-jwt:3.0.3")
-    implementation("io.ktor:ktor-server-sessions-jvm:3.0.3")
-    implementation("io.ktor:ktor-client-apache-jvm:3.0.3")
-    implementation("io.ktor:ktor-serialization-jackson:3.0.3")
+    implementation("io.ktor:ktor-server-status-pages")
+    implementation("io.ktor:ktor-server-default-headers")
+    implementation("io.ktor:ktor-server-cors")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm")
+    implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-server-host-common-jvm")
+    implementation("io.ktor:ktor-server-auth")
+    implementation("io.ktor:ktor-server-auth-jwt")
+    implementation("io.ktor:ktor-server-sessions-jvm")
+    implementation("io.ktor:ktor-client-apache-jvm")
+    implementation("io.ktor:ktor-serialization-jackson")
 
     implementation("org.apache.poi:poi-ooxml:5.3.0")
-    implementation("io.ktor:ktor-client-core-jvm:3.0.3")
-    implementation("io.ktor:ktor-client-logging-jvm:3.0.3")
+    implementation("io.ktor:ktor-client-core-jvm")
+    implementation("io.ktor:ktor-client-logging-jvm")
 
     implementation("org.simplejavamail:simple-java-mail:8.11.1")
 
-    testImplementation("io.ktor:ktor-server-test-host:3.0.3")
+    testImplementation("io.ktor:ktor-server-test-host")
 }
 
 tasks.register("buildAll") {
